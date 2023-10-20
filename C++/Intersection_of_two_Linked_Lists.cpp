631ccddac6c89d2baba8a1b987471d3567a3eb88
heeba-khan <heebak45@gmail.com>
2023-10-20 21:51:28 +0530

    Added the code

---
+++ C++/Intersection_of_two_Linked_Lists.cpp
@@ -0,0 +1,19 @@
+// Problem Link: https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+class Solution {
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *temp;
+		while(headA != NULL){
+			temp = headB;
+			while(temp != NULL){
+				if(headA == temp){
+					return headA;
+				}
+				temp = temp -> next;
+			}
+			headA = headA -> next;
+		}
+		return NULL;
+	}
+};
