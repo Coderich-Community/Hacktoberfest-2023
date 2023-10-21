f536c0ab9e26edee3840fbbc3ee163c1644132a6
Rishabh Dhawad <75854893+RishabhDhawad@users.noreply.github.com>
2023-10-21 16:57:47 +0530

    Update StoreAPolynomial.cpp
    Signed-off-by: Rishabh Dhawad <75854893+RishabhDhawad@users.noreply.github.com>

--- C++/StoreAPolynomial.cpp
+++ C++/StoreAPolynomial.cpp
@@ -1,71 +1,61 @@
-// Storing a Polynomial using Linked List
-
 #include <iostream>
 using namespace std;
 
-// Structure to represent a term in the polynomial
 struct Term {
     int coefficient;
     int exponent;
     Term* next;
 };
 
-class Polynomial {
-private:
-    Term* head; // Pointer to the first term in the polynomial
+void insertTerm(Term*& head, int coefficient, int exponent) {
+    Term* newTerm = new Term;
+    newTerm->coefficient = coefficient;
+    newTerm->exponent = exponent;
+    newTerm->next = nullptr;
 
-public:
-    Polynomial() {
-        head = nullptr;
-    }
-
-    // Function to insert a new term into the polynomial
-    void insertTerm(int coefficient, int exponent) {
-        Term* newTerm = new Term;
-        newTerm->coefficient = coefficient;
-        newTerm->exponent = exponent;
-        newTerm->next = nullptr;
-
-        if (head == nullptr || exponent > head->exponent) {
-            newTerm->next = head;
-            head = newTerm;
-        } else {
-            Term* current = head;
-            while (current->next != nullptr && current->next->exponent >= exponent) {
-                current = current->next;
-            }
-            newTerm->next = current->next;
-            current->next = newTerm;
-        }
-    }
-
-    // Function to display the polynomial
-    void displayPolynomial() {
+    if (!head) {
+        head = newTerm;
+    } else {
         Term* current = head;
-        while (current != nullptr) {
-            cout << current->coefficient << "x^" << current->exponent;
-            if (current->next != nullptr) {
-                cout << " + ";
-            }
+        Term* prev = nullptr;
+
+        while (current && current->exponent > exponent) {
+            prev = current;
             current = current->next;
         }
-        cout << endl;
+
+        if (current && current->exponent == exponent) {
+            current->coefficient += coefficient;
+            delete newTerm;
+        } else {
+            newTerm->next = current;
+            if (prev) {
+                prev->next = newTerm;
+            } else {
+                head = newTerm;
+            }
+        }
     }
-};
-
-int main() {
-    Polynomial p;
-
-    p.insertTerm(5, 2);
-    p.insertTerm(3, 1);
-    p.insertTerm(7, 4);
-    p.insertTerm(2, 3);
-
-    cout << "Polynomial: ";
-    p.displayPolynomial();
-
-    return 0;
 }
 
-// Output: 
-// Polynomial: 7x^4 + 2x^3 + 5x^2 + 3x^1
+void displayPolynomial(Term* head) {
+    Term* current = head;
+    while (current) {
+        cout << current->coefficient << "x^" << current->exponent;
+        if (current->next) {
+            cout << " + ";
+        }
+        current = current->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Term* polynomial = nullptr;
+
+    insertTerm(polynomial, 5, 2);
+    insertTerm(polynomial, 3, 1);
+    cout << "Polynomial: ";
+    displayPolynomial(polynomial);
+    return 0;
+}
